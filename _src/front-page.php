0ffe81319e8184501a6a799ab246691c08105a95
ほし <cgastrogation@gmail.com>
2024-06-17 17:58:41 +0900

    [add]_ファーストビュー作成

--- _src/front-page.php
+++ _src/front-page.php
@@ -1,5 +1,14 @@
 <?php get_header(); ?>
   <main id="main">
-
+    <div class="p-fv">
+      <div class="p-fv__inner">
+        <div class="p-fv__contents">
+          <h2 class="p-fv__lead">
+            <span class="p-fv__lead--1st">運転を、</span>
+            <span class="p-fv__lead--2nd">あなたの日常に</span>
+          </h2>
+        </div>
+      </div>
+    </div>
   </main>
 <?php get_footer(); ?>
