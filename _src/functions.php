78dd6e95179169b8803f8725cacaf1820942880f
ほし <cgastrogation@gmail.com>
2024-07-01 17:32:44 +0900

    [update]_metaタグ調整

--- _src/functions.php
+++ _src/functions.php
@@ -75,3 +75,98 @@ function wpcf7_autop_return_false()
 {
   return false;
 }
+
+/*********************
+OGPタグ/Twitterカード設定を出力
+*********************/
+function my_meta_ogp() {
+  if( is_front_page() || is_home() || is_singular() ){
+    global $post;
+    $ogp_title = '';
+    $ogp_descr = '';
+    $ogp_url = '';
+    $ogp_img = '';
+    $insert = '';
+
+    if( is_singular() ) { //記事＆固定ページ
+       setup_postdata($post);
+       $ogp_title = $post->post_title;
+       $ogp_descr = mb_substr(get_the_excerpt(), 0, 100);
+       $ogp_url = get_permalink();
+       wp_reset_postdata();
+    } elseif ( is_front_page() || is_home() ) { //トップページ
+       $ogp_title = get_bloginfo('name');
+       $ogp_descr = get_bloginfo('description');
+       $ogp_url = home_url();
+    }
+
+    //og:type
+    $ogp_type = ( is_front_page() || is_home() ) ? 'website' : 'article';
+
+    //og:image
+    if ( is_singular() && has_post_thumbnail() ) {
+       $ps_thumb = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full');
+       $ogp_img = $ps_thumb[0];
+    } else {
+     $ogp_img = home_url().'/assets/images/common/ogp_default.jpg';
+    }
+
+    //出力するOGPタグをまとめる
+    $insert .= '<meta property="og:title" content="'.esc_attr($ogp_title).'" />' . "\n";
+    $insert .= '<meta property="og:description" content="'.esc_attr($ogp_descr).'" />' . "\n";
+    $insert .= '<meta property="og:type" content="'.$ogp_type.'" />' . "\n";
+    $insert .= '<meta property="og:url" content="'.esc_url($ogp_url).'" />' . "\n";
+    $insert .= '<meta property="og:image" content="'.esc_url($ogp_img).'" />' . "\n";
+    $insert .= '<meta property="og:site_name" content="'.esc_attr(get_bloginfo('name')).'" />' . "\n";
+    $insert .= '<meta name="twitter:card" content="summary_large_image" />' . "\n";
+    //$insert .= '<meta name="twitter:site" content="@squid_jt" />' . "\n";
+    $insert .= '<meta property="og:locale" content="ja_JP" />' . "\n";
+
+    //facebookのapp_id（設定する場合）
+    //$insert .= '<meta property="fb:app_id" content="ここにappIDを入力">' . "\n";
+    //app_idを設定しない場合ここまで消す
+
+    echo $insert;
+  }
+} //END my_meta_ogp
+
+add_action('wp_head','my_meta_ogp');//headにOGPを出力
+
+add_action('wp_head', 'add_wp_head');
+
+function add_wp_head() {
+  /* favicon */
+  echo '<link rel="shortcut icon" href="' . get_template_directory_uri() . '/assets/images/favicon.ico" type="image/x-icon" />' . "\n";
+  /* pingback */
+  if ( is_singular() && pings_open() ) {
+      printf( '<link rel="pingback" href="%s" />' . "\n", esc_url( get_bloginfo( 'pingback_url' ) ) );
+  }
+}
+
+
+add_action( 'after_setup_theme', 'my_setup' );
+
+function my_setup() {
+    load_theme_textdomain( 'blankslate', get_template_directory() . '/languages' );
+    add_theme_support( 'title-tag' );
+    //add_theme_support( 'automatic-feed-links' );
+    add_theme_support( 'post-thumbnails' );
+    add_theme_support( 'html5', array( 'search-form' ) );
+}
+
+
+add_filter( 'document_title_separator', 'my_document_title_separator' );
+
+function my_document_title_separator( $sep ) {
+    $sep = '|';
+    return $sep;
+}
+add_filter( 'the_title', 'my_title' );
+
+function my_title( $title ) {
+    if ( $title == '' ) {
+        return '...';
+    } else {
+        return $title;
+    }
+}
